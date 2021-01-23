4a1c85e04585ed1c6cf051c289a64c89bb7f7e77
Freddie Fryer <freddiefryercode@gmail.com>
2021-01-23 11:24:03 +0000

    have now listed all tasks in challenge 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  Task.create(
+    title: Faker::Job.field,
+    details: Faker::Lorem.sentence(word_count: 6),
+    completed: false
+  )
+end
